5da00f0157fb1c47dbe89d137c064eae9a093028
zzzyzh <zzzyzh@qq.com>
2024-04-09 12:09:10 +0800

    Update

--- scripts/infer_box.sh
+++ scripts/infer_box.sh
@@ -12,4 +12,10 @@
 
 # sam_med2d
 # base
-# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam_med2d --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/sam-med2d/sam-med2d_b.pth --prompt box --strategy base
+# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam_med2d --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/sam-med2d_b.pth --prompt box --strategy base
+
+#############################
+
+# sam_med2d
+# base
+CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode med_sam --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/medsam_vit_b.pth --image_size 1024 --prompt box --strategy base
--- scripts/infer_point.sh
+++ scripts/infer_point.sh
@@ -10,9 +10,9 @@
 # CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/sam_vit_b_01ec64.pth --image_size 1024 --prompt point --strategy far --iter_point 10
 
 # m_area
-# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/sam_vit_b_01ec64.pth --image_size 1024 --prompt point --strategy m_area --iter_point 1
-# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/sam_vit_b_01ec64.pth --image_size 1024 --prompt point --strategy m_area --iter_point 5
-# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/sam_vit_b_01ec64.pth --image_size 1024 --prompt point --strategy m_area --iter_point 10
+CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/sam_vit_b_01ec64.pth --image_size 1024 --prompt point --strategy m_area --iter_point 1
+CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/sam_vit_b_01ec64.pth --image_size 1024 --prompt point --strategy m_area --iter_point 5
+CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/sam_vit_b_01ec64.pth --image_size 1024 --prompt point --strategy m_area --iter_point 10
 
 #############################
 
@@ -28,24 +28,42 @@
 # CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam --model_type vit_h --sam_checkpoint /home/yanzhonghao/data/ven/weights/sam_vit_h_4b8939.pth --image_size 1024 --prompt point --strategy far --iter_point 10
 
 # m_area
-# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam --model_type vit_h --sam_checkpoint /home/yanzhonghao/data/ven/weights/sam_vit_h_4b8939.pth --image_size 1024 --prompt point --strategy m_area --iter_point 1
-# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam --model_type vit_h --sam_checkpoint /home/yanzhonghao/data/ven/weights/sam_vit_h_4b8939.pth --image_size 1024 --prompt point --strategy m_area --iter_point 5
-# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam --model_type vit_h --sam_checkpoint /home/yanzhonghao/data/ven/weights/sam_vit_h_4b8939.pth --image_size 1024 --prompt point --strategy m_area --iter_point 10
+CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam --model_type vit_h --sam_checkpoint /home/yanzhonghao/data/ven/weights/sam_vit_h_4b8939.pth --image_size 1024 --prompt point --strategy m_area --iter_point 1
+CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam --model_type vit_h --sam_checkpoint /home/yanzhonghao/data/ven/weights/sam_vit_h_4b8939.pth --image_size 1024 --prompt point --strategy m_area --iter_point 5
+CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam --model_type vit_h --sam_checkpoint /home/yanzhonghao/data/ven/weights/sam_vit_h_4b8939.pth --image_size 1024 --prompt point --strategy m_area --iter_point 10
 
 #############################
 
 # sam_med2d
 # base
-# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam_med2d --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/sam-med2d/sam-med2d_b.pth --prompt point --strategy base --iter_point 1 --mask_prompt True
-# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam_med2d --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/sam-med2d/sam-med2d_b.pth --prompt point --strategy base --iter_point 5 --mask_prompt True
-# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam_med2d --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/sam-med2d/sam-med2d_b.pth --prompt point --strategy base --iter_point 10 --mask_prompt True
+# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam_med2d --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/sam-med2d_b.pth --prompt point --strategy base --iter_point 1 --mask_prompt True
+# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam_med2d --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/sam-med2d_b.pth --prompt point --strategy base --iter_point 5 --mask_prompt True
+# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam_med2d --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/sam-med2d_b.pth --prompt point --strategy base --iter_point 10 --mask_prompt True
 
 # far
-# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam_med2d --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/sam-med2d/sam-med2d_b.pth --prompt point --strategy far --iter_point 1 --mask_prompt True
-# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam_med2d --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/sam-med2d/sam-med2d_b.pth --prompt point --strategy far --iter_point 5 --mask_prompt True
-# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam_med2d --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/sam-med2d/sam-med2d_b.pth --prompt point --strategy far --iter_point 10 --mask_prompt True
+# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam_med2d --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/sam-med2d_b.pth --prompt point --strategy far --iter_point 1 --mask_prompt True
+# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam_med2d --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/sam-med2d_b.pth --prompt point --strategy far --iter_point 5 --mask_prompt True
+# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam_med2d --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/sam-med2d_b.pth --prompt point --strategy far --iter_point 10 --mask_prompt True
 
 # m_area
-# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam_med2d --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/sam-med2d/sam-med2d_b.pth --prompt point --strategy m_area --iter_point 1 --mask_prompt True
-# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam_med2d --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/sam-med2d/sam-med2d_b.pth --prompt point --strategy m_area --iter_point 5 --mask_prompt True
-# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam_med2d --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/sam-med2d/sam-med2d_b.pth --prompt point --strategy m_area --iter_point 10 --mask_prompt True
+CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam_med2d --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/sam-med2d_b.pth --prompt point --strategy m_area --iter_point 1 --mask_prompt True
+CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam_med2d --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/sam-med2d_b.pth --prompt point --strategy m_area --iter_point 5 --mask_prompt True
+CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode sam_med2d --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/sam-med2d_b.pth --prompt point --strategy m_area --iter_point 10 --mask_prompt True
+
+#############################
+
+# med_sam
+# base
+# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode med_sam --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/medsam_vit_b.pth --image_size 1024 --prompt point --strategy base --iter_point 1 --mask_prompt True
+# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode med_sam --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/medsam_vit_b.pth --image_size 1024 --prompt point --strategy base --iter_point 5 --mask_prompt True
+# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode med_sam --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/medsam_vit_b.pth --image_size 1024 --prompt point --strategy base --iter_point 10 --mask_prompt True
+
+# far
+# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode med_sam --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/medsam_vit_b.pth --image_size 1024 --prompt point --strategy far --iter_point 1 --mask_prompt True
+# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode med_sam --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/medsam_vit_b.pth --image_size 1024 --prompt point --strategy far --iter_point 5 --mask_prompt True
+# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode med_sam --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/medsam_vit_b.pth --image_size 1024 --prompt point --strategy far --iter_point 10 --mask_prompt True
+
+# m_area
+# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode med_sam --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/medsam_vit_b.pth --image_size 1024 --prompt point --strategy m_area --iter_point 1 --mask_prompt True
+# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode med_sam --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/medsam_vit_b.pth --image_size 1024 --prompt point --strategy m_area --iter_point 5 --mask_prompt True
+# CUDA_VISIBLE_DEVICES=0 python test.py --sam_mode med_sam --model_type vit_b --sam_checkpoint /home/yanzhonghao/data/ven/weights/medsam_vit_b.pth --image_size 1024 --prompt point --strategy m_area --iter_point 10 --mask_prompt True
